42fa77f836f41501b750377468a40a61d91c10a4
yyiridescent <124308478+yyiridescent@users.noreply.github.com>
2023-03-11 19:24:20 +0800

    Add files via upload

---
+++ homework_new_new_new/ready.py
@@ -0,0 +1,53 @@
+from flask import Flask, config
+from flask_sqlalchemy import SQLAlchemy
+from werkzeug.security import generate_password_hash, check_password_hash
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:yueye13084030!@gz-cynosdbmysql-grp-ro694ctz.sql.tencentcdb.com:28492/music?charset=utf8'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config.from_object(config)
+
+db = SQLAlchemy(app)
+
+class User_History(db.Model):  # 历史记录
+    __tablename__ = 'user_history'
+    id = db.Column(db.Integer, db.ForeignKey('user_message.id'), primary_key=True)
+    history = db.Column(db.Integer, db.ForeignKey('history.id'), primary_key=True)  # 外键
+    fav = db.Column(db.Integer, nullable=False, default=0)  # default默认值
+
+
+class User_message(db.Model):  # 登录&注册
+    __tablename__ = 'user_message'
+    id = db.Column(db.Integer, primary_key=True)  # integer 整型 32位
+    username = db.Column(db.String(255), nullable=False)
+    password_hash = db.Column(db.String(255), nullable=False)
+    history = db.relationship('History', secondary='user_history', lazy='subquery',backref=db.backref('user', lazy=True))  # relationship建立一对多（一）关系,使用子查询
+
+    @property
+    def password(self):
+        return self.password_hash
+
+    @password.setter
+    def password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, checkpassword):
+        return check_password_hash(self.password_hash, checkpassword)
+
+class History(db.Model):  # 历史记录的具体内容
+    __tablename__ = 'history'
+    name = db.Column(db.String(255), nullable=False)
+    artist = db.Column(db.String(255), nullable=False)
+    album = db.Column(db.String(255), nullable=False)
+    duration = db.Column(db.String(255), nullable=False)
+    rid = db.Column(db.Integer, nullable=False)
+    id = db.Column(db.Integer, primary_key=True)
+
+with app.app_context():
+    db.drop_all()
+    db.create_all()
+    name = User_message.query.filter(User_message.username == 'admin').first()
+    if not name:
+        admin = User_message(username='admin', password='123456')
+        db.session.add(admin)
+        db.session.commit()
---
+++ homework_new_new_new/search.py
@@ -0,0 +1,116 @@
+import os
+import requests
+from flask import jsonify, Blueprint, request, send_file
+from ready import User_message,db,app,History
+from user import Authorized
+
+def info(rid):
+    detailurl = f'http://m.kuwo.cn/newh5/singles/songinfoandlrc?musicId={rid}&httpsStatus=1'
+    detail = requests.get(detailurl).json()['data']['songinfo']
+    return detail
+
+new_search=Blueprint('search',__name__)
+
+@new_search.route('/search',methods=['GET'])
+def Search():
+    authorization = request.headers.get('Authorization')
+    text = request.args.get('text')
+    page = request.args.get('page')
+    print(1)
+    if not authorization:
+        return jsonify(code=404,message='请先登录'),200
+    else:
+        token=Authorized(authorization)
+
+    if not text:
+        return jsonify(code=404,message='请先输入'),200
+
+    elif token:
+
+        url = 'http://www.kuwo.cn/api/www/search/searchMusicBykeyWord'
+        headers1 = {
+            'Cookie': 'kw_token=XFXKUH6WB',
+            'csrf': 'XFXKUH6WB',
+            'Referer': 'http://www.kuwo.cn/search/list',
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36 Edg/110.0.1587.41'
+        }
+        params = {
+            'key': text,
+            'pn': page,
+            'rn': 10,
+            'httpsStatus': '1',
+            'reqId': '53027fb0 - bfb2 - 11ed - a428 - 7fb8c7b7de6e'
+        }
+        response = requests.get(url=url, headers=headers1, params=params)
+        json = response.json()
+        total = int(json['data']['total'])
+        if total == 0:
+            return jsonify(code=404, message=f'没有找到与{text}有关的内容'), 200
+        if total % 10 == 0:
+            pages = int(total / 10)
+        else:
+            pages = int(total / 10) + 1
+        list1=[]
+        json_list = json['data']['list']
+        print(json_list)
+        for music in json_list:
+            rid = music['rid']
+            message = info(rid)
+            dict = {
+                "name": message['songName'],
+                "artist": message['artist'],
+                "album": message['album'],
+                "duration": message['songTimeMinutes'],
+                "rid": message['id']
+            }
+            list1 += [dict]
+        music_list = {'list': list1}
+        return jsonify(code=200, message='success', data=music_list), 200
+    else:
+        return jsonify(code=404, message='登录认证失败，请重新登录'), 200
+
+@new_search.route('/search/download/<rid>',methods=['GET'])
+def download(rid):
+    try:
+        os.makedirs('Music')
+    except Exception as e:
+        print(e)
+    detail=info(rid)
+    authorization = request.headers.get('Authorization')
+    if not authorization:
+        return jsonify(code=404,message='请先登录'),200
+    token=Authorized(authorization)
+    id=token.get('id')
+    if not token:
+        return jsonify(code=404,message='请重新登录'),200
+    if token:
+        try:
+            with app.app_context():
+                user=User_message.query.get(id)
+                query=History.query.filter(History.rid == rid).first()
+                if not query:
+                    add=History(name=detail['songName'],artist=detail['artist'],album=detail['album'],duration=detail['songTimeMinutes'],rid=detail['id'])
+                    db.session.add(add)
+                    user.history.append(add)
+                    db.session.commit()
+                else:
+                    user.history.append(query)
+                    db.session.commit()
+
+                title=f'{detail["songName"]}-{detail["artist"]}'
+                file_path=f'music/{title}.mp3'
+                if os.path.exists(file_path):
+                    return send_file(file_path,as_attachment=True)
+                else:
+                    infourl=f'http://www.kuwo.cn/api/v1/www/music/playUrl?mid={rid}&type=convert_url3&br=320kmp3'
+                    musicurl=requests.get(infourl).json()['data']['url']
+                    musicdata=requests.get(musicurl).content
+                    with open(file_path,mode='wb') as f:
+                        f.write(musicdata)
+                    return send_file(file_path,as_attachment=True)
+        except Exception as e:
+            return jsonify(code=404,message='{e}'),200
+
+@new_search.errorhandler(Exception)
+def error(e):
+    return jsonify(code=200, message='请重新登录'), 200
